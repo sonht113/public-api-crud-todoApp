4677d244fcf09144991cd2d778a3b99b7f7fd7ae
trongson <trongson18101999@gmail.com>
2024-06-22 01:53:30 +0700

    fix: fix api db

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ const PORT = 3000
 mongoose.set('strictQuery', false)
 mongoose
   .connect("mongodb+srv://nuicoder:coder123@cluster0.emxawoq.mongodb.net/todo")
-  .then(() => app.listen(PORT, () => console.log(`Server running: ${PORT}`)))
+  .then(() => app.listen(PORT, "0.0.0.0", () => console.log(`Server running: ${PORT}`)))
   .catch((error) => console.log(error))
